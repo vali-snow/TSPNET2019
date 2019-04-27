3eb164a7756468a47112201305616f080f094a22
vali-snow <vali-snow@users.noreply.github.com>
2019-04-27 15:21:51 +0300

    host close + trycatch

--- ServiceAutoWcfHost/Program.cs
+++ ServiceAutoWcfHost/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ServiceAutoWcfServiceLibrary;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.ServiceModel;
 using System.Text;
@@ -12,7 +13,9 @@ namespace ServiceAutoWcfHost
     {
         static void Main(string[] args)
         {
-            var hosts = new List<ServiceHost>() {
+            try
+            {
+                var hosts = new List<ServiceHost>() {
                 new ServiceHost(typeof(SAAuto)),
                 new ServiceHost(typeof(SAClient)),
                 new ServiceHost(typeof(SAComanda)),
@@ -23,13 +26,24 @@ namespace ServiceAutoWcfHost
                 new ServiceHost(typeof(SAOperatie)),
                 new ServiceHost(typeof(SASasiu)),
             };
-            foreach (var host in hosts)
-            {
-                host.Open();
-            }
+                foreach (var host in hosts)
+                {
+                    host.Open();
+                }
 
-            Console.WriteLine("Hit any key to exit");
-            Console.ReadKey();
+                Console.WriteLine("Hit any key to exit");
+                Console.ReadKey();
+
+                foreach (var host in hosts)
+                {
+                    host.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            
         }
     }
 }
