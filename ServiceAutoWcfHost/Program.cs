fbb19370e42e02a7fa99bcdc6ddc97335f92107f
vali-snow <vali-snow@users.noreply.github.com>
2019-04-27 14:58:38 +0300

    wcf self host project finished

---
+++ ServiceAutoWcfHost/Program.cs
@@ -0,0 +1,35 @@
+﻿using ServiceAutoWcfServiceLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceAutoWcfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hosts = new List<ServiceHost>() {
+                new ServiceHost(typeof(SAAuto)),
+                new ServiceHost(typeof(SAClient)),
+                new ServiceHost(typeof(SAComanda)),
+                new ServiceHost(typeof(SADetaliiComanda)),
+                new ServiceHost(typeof(SAImagine)),
+                new ServiceHost(typeof(SAMaterial)),
+                new ServiceHost(typeof(SAMecanic)),
+                new ServiceHost(typeof(SAOperatie)),
+                new ServiceHost(typeof(SASasiu)),
+            };
+            foreach (var host in hosts)
+            {
+                host.Open();
+            }
+
+            Console.WriteLine("Hit any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
